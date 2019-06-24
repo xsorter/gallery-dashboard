aa0cb1ce560ee5bf4a80d630481cb492a1f2ec9c
Oleksii Nelin <xsorter@gmail.com>
2019-06-25 01:05:28 +0300

    current year added

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  currentYear: number;
   constructor() { }
 
   ngOnInit() {
+    this.currentYear = new Date().getFullYear();
   }
 
 }
