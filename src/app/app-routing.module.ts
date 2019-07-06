d12a6b4a363e6242d982404c1d84d5dc634f5312
Oleksii Nelin <xsorter@gmail.com>
2019-07-06 16:52:12 +0300

    admin route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './client/home/home.component';
 import { ListComponent } from './client/list/list.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { DetailComponent } from './client/detail/detail.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'list', component: ListComponent},
   {path: 'list/:id', component: DetailComponent},
+  {path: 'admin', component: DashboardComponent},
   {path: '**', component: NotFoundComponent}
 ];
 
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     
