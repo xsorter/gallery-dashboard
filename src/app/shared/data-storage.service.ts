a57639c47a7becff6ce4017f0a49164af3bee1dc
Oleksii Nelin <xsorter@gmail.com>
2019-07-22 00:31:59 +0300

    add material card to list component

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,9 +4,9 @@ import { Article } from './article.model';
 @Injectable()
 export class DataStorageService {
   public articles: Article[] = [
-    new Article('Title-1', 'Some description-1', 'some image-1'),
-    new Article('Title-2', 'Some description-2', 'some image-2'),
-    new Article('Title-3', 'Some description-3', 'some image-3'),
+    new Article('Title-1', 'Some description-1', 'https://images.unsplash.com/photo-1563714272638-882a6309ba7d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'),
+    new Article('Title-2', 'Some description-2', 'https://images.unsplash.com/photo-1563573475337-918ce3ee6fc2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'),
+    new Article('Title-3', 'Some description-3', 'https://images.unsplash.com/photo-1563441846967-d5c0d86b20a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80'),
   ]
 
   constructor(){}
