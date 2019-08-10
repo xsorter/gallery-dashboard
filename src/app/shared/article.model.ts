d3988934c89f5a5600dfdf22765908a7c48893e7
Oleksii Nelin <xsorter@gmail.com>
2019-08-10 14:24:08 +0300

    model changed

--- src/app/shared/article.model.ts
+++ src/app/shared/article.model.ts
@@ -1,9 +1,9 @@
 export class Article {
   public title: string;
   public description: string;
-  public coverPath: string;
+  public coverPath ?: Array<string>;
 
-  constructor(title: string, description: string, coverPath: string){
+  constructor(title: string, description: string, coverPath: Array<string>){
     this.title = title;
     this.description = description;
     this.coverPath = coverPath;
