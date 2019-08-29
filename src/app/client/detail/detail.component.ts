746cb661721b7e8a27dcc1762451aeb7d7b05b09
Oleksii Nelin <xsorter@gmail.com>
2019-08-29 23:28:42 +0300

    single article type added

--- src/app/client/detail/detail.component.ts
+++ src/app/client/detail/detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ɵConsole } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataStorageService } from 'src/app/shared/data-storage.service';
 import { Article } from 'src/app/shared/article.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-detail',
@@ -12,18 +13,16 @@ export class DetailComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private dataService: DataStorageService) { }
 
-  //TODO: type missing
-  article;
+  article: Observable<Article>;
 
   //TODO: refactor
   ngOnInit() {
     this.route.params.subscribe(
       (data) => {
         this.dataService.getDatabaseArticle(data.id)
-          .subscribe(doc => {
-            this.article = doc
-          })
-        /*this.article = this.dataService.getDatabaseArticle(data.id);*/
+        .subscribe(doc => {
+          this.article = doc
+        })
       }
     )
   }
