c7461ca68f76f8faf89d3bccb1b9f8dd73cb9c98
xsorter <xsorter@gmail.com>
2019-09-01 11:31:40 +0300

    added upload task for images

--- src/app/admin/upload-progress/upload-progress.component.ts
+++ src/app/admin/upload-progress/upload-progress.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { tap, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-upload-progress',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload-progress.component.scss']
 })
 export class UploadProgressComponent implements OnInit {
+  
+  @Input() file: File;
 
-  constructor() { }
+  task: AngularFireUploadTask;
+  percentage: Observable<number>;
+  snapshot: Observable<any>;
+  downloadUrl: string;
+
+
+  constructor(private storage: AngularFireStorage, private db: AngularFirestore) { }
 
   ngOnInit() {
   }
 
+  startUpload(){
+    const path = `articles/${Date.now}_${this.file.name}`;
+    const ref = this.storage.ref(path);
+
+    this.task = this.storage.upload(path, this.file);
+    this.percentage = this.task.percentageChanges();
+
+    this.snapshot = this.task.snapshotChanges()
+      .pipe(
+        tap(console.log),
+        finalize( async() => {
+          this.downloadUrl = await ref.getDownloadURL().toPromise();
+
+          /*
+          TODO: save url to form values
+          this.db.collection('files').add( { downloadURL: this.downloadUrl, path });*/
+        })
+      )
+
+  }
+
+  isActive(snapshot){
+    return snapshot.state === 'running' && snapshot.bytesTransferred < snapshot.totalBytes
+  }
+  
 }
