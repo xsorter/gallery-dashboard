2afe7a104fb280fa770593e3660c1f035973818f
Oleksii Nelin <xsorter@gmail.com>
2019-09-06 00:39:50 +0300

    added dynamic images for gallery

--- src/app/client/gallery/gallery.component.ts
+++ src/app/client/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+  @Input() images: String[];
 
   constructor() { }
 
   ngOnInit() {
+    console.log('IMGS',this.images)
   }
 
   layout: boolean = false;
