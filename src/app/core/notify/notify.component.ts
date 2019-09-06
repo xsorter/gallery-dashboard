b4556271f9c559061d4b1069bdb8ac8844163fb8
Oleksii Nelin <xsorter@gmail.com>
2019-09-07 01:44:29 +0300

    animation added

--- src/app/core/notify/notify.component.ts
+++ src/app/core/notify/notify.component.ts
@@ -7,7 +7,19 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 @Component({
   selector: 'app-notify',
   templateUrl: './notify.component.html',
-  styleUrls: ['./notify.component.scss']
+  styleUrls: ['./notify.component.scss'],
+  animations: [trigger('notyState', [
+      state('false', style({
+        opacity: '0',
+        transform: 'scale(1)'
+      })),
+      state('true', style({
+        opacity: '1',
+        transform: 'scale(1)'
+      })),
+      transition('*=>false', animate('900ms')),
+      transition('*=>true', animate('1200ms'))
+  ])]
 })
 export class NotifyComponent implements OnInit, OnDestroy {
 
@@ -15,14 +27,21 @@ export class NotifyComponent implements OnInit, OnDestroy {
 
   notification: Notify;
   subsciption: Subscription;
+  animationState: boolean = false;
 
   ngOnInit() {
     this.subsciption = this.notifyService.notify$.subscribe(
       (item) => {
         this.notification = item;
+        this.onChangeState(this.notification.fired);
         console.log('GETTING', item)
       }
     )
+  }
+
+  onChangeState(state: boolean){
+    this.animationState = state;
+    console.log('ANIMATION STATE', state)
   }
 
   ngOnDestroy() {
