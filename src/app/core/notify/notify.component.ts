a8442c75ecc20acf2b32a8607dc93809ff7c5051
Oleksii Nelin <xsorter@gmail.com>
2019-09-07 00:24:59 +0300

    animations import added

--- src/app/core/notify/notify.component.ts
+++ src/app/core/notify/notify.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NotifyService } from 'src/app/shared/notify-service';
 import { Subscription } from 'rxjs';
 import { Notify } from 'src/app/shared/notify.model';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-notify',
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -11,7 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatSelectModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
   ],
   exports: [
     MatButtonModule,
@@ -21,7 +21,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatSelectModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
   ],
 })
 export class SharedMaterialModule { }
